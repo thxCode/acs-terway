AliyunContainerService/terway

add terway controller
Signed-off-by: l1b0k <libokang.dev@gmail.com>

cmd/terway-controlplane/terway-controlplane.go
@@ -1,0 +1,188 @@
+/*
+Copyright 2021 Terway Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	goflag "flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/AliyunContainerService/terway/pkg/aliyun"
+	"github.com/AliyunContainerService/terway/pkg/apis/crds"
+	networkv1beta1 "github.com/AliyunContainerService/terway/pkg/apis/network.alibabacloud.com/v1beta1"
+	"github.com/AliyunContainerService/terway/pkg/cert"
+	register "github.com/AliyunContainerService/terway/pkg/controller"
+	_ "github.com/AliyunContainerService/terway/pkg/controller/all"
+	"github.com/AliyunContainerService/terway/pkg/controller/vswitch"
+	"github.com/AliyunContainerService/terway/pkg/controller/webhook"
+	"github.com/AliyunContainerService/terway/pkg/logger"
+	"github.com/AliyunContainerService/terway/pkg/utils"
+	"github.com/AliyunContainerService/terway/types"
+
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/pkg/version"
+	"k8s.io/klog/v2/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+)
+
+var (
+	leaseLockName      string
+	leaseLockNamespace string
+	healthzBindAddress string
+
+	scheme = runtime.NewScheme()
+	log    = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	flag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+
+	flag.StringVar(&leaseLockName, "lease-lock-name", "terway-controller-lock", "the lease lock resource name")
+	flag.StringVar(&leaseLockNamespace, "lease-lock-namespace", "kube-system", "the lease lock resource namespace")
+	flag.StringVar(&healthzBindAddress, "healthzBindAddress", "0.0.0.0:80", "for health check")
+
+	flag.String("cluster-id", "", "cluster id for the cluster")
+	flag.String("vpc-id", "", "vpc id for the cluster")
+
+	flag.String("cluster-domain", "cluster.local", "domain for the cluster")
+	flag.String("controller-namespace", "kube-system", "specific controller run namespace")
+	flag.String("controller-name", "terway-controlplane", "specific controller name")
+	flag.String("cert-dir", "/var/run/webhook-cert", "webhook cert dir")
+	flag.Int("webhook-port", 443, "port for webhook")
+
+	_ = viper.BindPFlag("cluster-id", flag.CommandLine.Lookup("cluster-id"))
+	_ = viper.BindPFlag("vpc-id", flag.CommandLine.Lookup("vpc-id"))
+
+	_ = viper.BindPFlag("cluster-domain", flag.CommandLine.Lookup("cluster-domain"))
+	_ = viper.BindPFlag("controller-namespace", flag.CommandLine.Lookup("controller-namespace"))
+	_ = viper.BindPFlag("controller-name", flag.CommandLine.Lookup("controller-name"))
+	_ = viper.BindPFlag("cert-dir", flag.CommandLine.Lookup("cert-dir"))
+	_ = viper.BindPFlag("webhook-port", flag.CommandLine.Lookup("webhook-port"))
+
+	_ = viper.BindPFlag("v", flag.CommandLine.Lookup("v"))
+
+	_ = viper.BindEnv("ALIBABA_CLOUD_REGION_ID")
+	_ = viper.BindEnv("ALIBABA_CLOUD_ACCESS_KEY_ID")
+	_ = viper.BindEnv("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
+
+	viper.SetDefault("v", 4)
+
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(networkv1beta1.AddToScheme(scheme))
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	if viper.GetInt("v") > len(logrus.AllLevels) {
+		_ = logger.SetLevel(logrus.AllLevels[len(logrus.AllLevels)-1].String())
+	} else {
+		_ = logger.SetLevel(logrus.AllLevels[viper.GetInt("v")].String())
+	}
+
+	ctrl.SetLogger(klogr.New())
+	log.Info(fmt.Sprintf("GitCommit %s BuildDate %s Platform %s",
+		version.Get().GitCommit, version.Get().BuildDate, version.Get().Platform))
+
+	flag.Parse()
+
+	utils.RegisterClients()
+
+	err := crds.RegisterCRDs()
+	if err != nil {
+		panic(err)
+	}
+
+	err = cert.SyncCert()
+	if err != nil {
+		panic(err)
+	}
+
+	err = types.ParseClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info("init cluster config", "clusterID", viper.GetString("cluster-id"), "vpcID", viper.GetString("vpc-id"))
+
+	aliyunClient, err := aliyun.NewAliyun(viper.GetString("ALIBABA_CLOUD_ACCESS_KEY_ID"), viper.GetString("ALIBABA_CLOUD_ACCESS_KEY_SECRET"), viper.GetString("ALIBABA_CLOUD_REGION_ID"), "")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := ctrl.SetupSignalHandler()
+	restConfig := ctrl.GetConfigOrDie()
+
+	mgr, err := ctrl.NewManager(restConfig, ctrl.Options{
+		Scheme:                     scheme,
+		HealthProbeBindAddress:     healthzBindAddress,
+		Host:                       "0.0.0.0",
+		Port:                       viper.GetInt("webhook-port"),
+		CertDir:                    viper.GetString("cert-dir"),
+		LeaderElection:             true,
+		LeaderElectionID:           uuid.New().String() + "-" + os.Getenv("K8S_POD_NAME"),
+		LeaderElectionNamespace:    viper.GetString("controller-namespace"),
+		LeaderElectionResourceLock: "leases",
+		MetricsBindAddress:         "0",
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = mgr.AddHealthzCheck("healthz", healthz.Ping)
+	if err != nil {
+		panic(err)
+	}
+	err = mgr.AddReadyzCheck("readyz", healthz.Ping)
+	if err != nil {
+		panic(err)
+	}
+
+	mgr.GetWebhookServer().Register("/mutating", webhook.MutatingHook(mgr.GetClient()))
+	mgr.GetWebhookServer().Register("/validate", webhook.ValidateHook(mgr.GetClient()))
+
+	vSwitchCtrl, err := vswitch.NewSwitchPool(aliyunClient)
+	if err != nil {
+		panic(err)
+	}
+	err = mgr.Add(vSwitchCtrl)
+	if err != nil {
+		panic(err)
+	}
+
+	for name := range register.Controllers {
+		err = register.Controllers[name](mgr, aliyunClient, vSwitchCtrl)
+		if err != nil {
+			panic(err)
+		}
+		log.Info("register controller", "controller", name)
+	}
+
+	log.Info("controller started")
+	err = mgr.Start(ctx)
+	if err != nil {
+		panic(err)
+	}
+}

pkg/cert/webhook.go
@@ -1,0 +1,217 @@
+package cert
+
+import (
+	"bytes"
+	"context"
+	cryptorand "crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/AliyunContainerService/terway/pkg/utils"
+
+	"github.com/spf13/viper"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+const (
+	serverCertKey = "tls.crt"
+	serverKeyKey  = "tls.key"
+
+	caCertKey = "ca.crt"
+)
+
+// SyncCert sync cert for webhook
+func SyncCert() error {
+
+	cs := utils.K8sClient
+	// check secret
+	var serverCertBytes, serverKeyBytes, caCertBytes []byte
+
+	ns := viper.GetString("controller-namespace")
+	name := viper.GetString("controller-name")
+
+	// get cert from secret or generate it
+	existSecret, err := cs.CoreV1().Secrets(ns).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		if !errors.IsNotFound(err) {
+			return fmt.Errorf("error get cert from secret, %w", err)
+		}
+		// create certs
+		s, err := GenerateCerts(ns, name, viper.GetString("cluster-domain"))
+		if err != nil {
+			return fmt.Errorf("error generate cert, %w", err)
+		}
+
+		serverCertBytes = s.Data[serverCertKey]
+		serverKeyBytes = s.Data[serverKeyKey]
+		caCertBytes = s.Data[caCertKey]
+		s.Name = name
+		s.Namespace = ns
+		// create secret this make sure one is the leader
+		_, err = cs.CoreV1().Secrets(ns).Create(context.Background(), s, metav1.CreateOptions{})
+		if err != nil {
+			if !errors.IsAlreadyExists(err) {
+				return fmt.Errorf("error create cert to secret, %w", err)
+			}
+			secret, err := cs.CoreV1().Secrets(ns).Get(context.Background(), name, metav1.GetOptions{})
+			if err != nil {
+				return fmt.Errorf("error get cert from secret, %w", err)
+			}
+			serverCertBytes = secret.Data[serverCertKey]
+			serverKeyBytes = secret.Data[serverKeyKey]
+			caCertBytes = secret.Data[caCertKey]
+		}
+	} else {
+		serverCertBytes = existSecret.Data[serverCertKey]
+		serverKeyBytes = existSecret.Data[serverKeyKey]
+		caCertBytes = existSecret.Data[caCertKey]
+	}
+
+	certDir := viper.GetString("cert-dir")
+	err = os.MkdirAll(certDir, os.ModeDir)
+	if err != nil {
+		return err
+	}
+
+	// write cert to file
+	err = os.WriteFile(filepath.Join(certDir, serverCertKey), serverCertBytes, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error create secret file, %w", err)
+	}
+	err = os.WriteFile(filepath.Join(certDir, serverKeyKey), serverKeyBytes, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error create secret file, %w", err)
+	}
+	err = os.WriteFile(filepath.Join(certDir, caCertKey), caCertBytes, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error create secret file, %w", err)
+	}
+
+	// update webhook
+	webhook, err := cs.AdmissionregistrationV1().MutatingWebhookConfigurations().Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	if len(webhook.Webhooks) == 0 {
+		return fmt.Errorf("no webhook config found")
+	}
+	newCfg := webhook.DeepCopy()
+	for i, hook := range webhook.Webhooks {
+		if len(hook.ClientConfig.CABundle) != 0 {
+			continue
+		}
+		// patch ca
+		newCfg.Webhooks[i].ClientConfig.CABundle = caCertBytes
+	}
+	patchBytes, err := json.Marshal(newCfg)
+	if err != nil {
+		return err
+	}
+	err = wait.ExponentialBackoff(utils.DefaultPatchBackoff, func() (done bool, err error) {
+		_, innerErr := cs.AdmissionregistrationV1().MutatingWebhookConfigurations().Patch(context.Background(), webhook.Name, types.StrategicMergePatchType, patchBytes, metav1.PatchOptions{})
+		if innerErr != nil {
+			return false, err
+		}
+		return true, nil
+	})
+	return err
+}
+
+func GenerateCerts(serviceNamespace, serviceName, clusterDomain string) (*corev1.Secret, error) {
+	var caPEM, serverCertPEM, serverPrivateKeyPEM *bytes.Buffer
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(2021),
+		Subject: pkix.Name{
+			Organization: []string{clusterDomain},
+		},
+		NotBefore:             time.Now().Add(-time.Hour),
+		NotAfter:              time.Now().AddDate(100, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	caPK, err := rsa.GenerateKey(cryptorand.Reader, 4096)
+	if err != nil {
+		return nil, err
+	}
+
+	caBytes, err := x509.CreateCertificate(cryptorand.Reader, ca, ca, &caPK.PublicKey, caPK)
+	if err != nil {
+		return nil, err
+	}
+
+	caPEM = new(bytes.Buffer)
+	err = pem.Encode(caPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: caBytes,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	commonName := fmt.Sprintf("%s.%s.svc", serviceName, serviceNamespace)
+	dnsNames := []string{serviceName,
+		fmt.Sprintf("%s.%s", serviceName, serviceNamespace),
+		commonName}
+
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(2021),
+		DNSNames:     dnsNames,
+		Subject: pkix.Name{
+			CommonName:   commonName,
+			Organization: []string{clusterDomain},
+		},
+		NotBefore:   time.Now().Add(-time.Hour),
+		NotAfter:    time.Now().AddDate(100, 0, 0),
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
+	}
+
+	serverPrivateKey, err := rsa.GenerateKey(cryptorand.Reader, 4096)
+	if err != nil {
+		return nil, err
+	}
+
+	serverCertBytes, err := x509.CreateCertificate(cryptorand.Reader, cert, ca, &serverPrivateKey.PublicKey, caPK)
+	if err != nil {
+		return nil, err
+	}
+
+	serverCertPEM = new(bytes.Buffer)
+	err = pem.Encode(serverCertPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: serverCertBytes,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	serverPrivateKeyPEM = new(bytes.Buffer)
+	err = pem.Encode(serverPrivateKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(serverPrivateKey),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &corev1.Secret{Data: map[string][]byte{
+		caCertKey:     caPEM.Bytes(),
+		serverCertKey: serverCertPEM.Bytes(),
+		serverKeyKey:  serverPrivateKeyPEM.Bytes(),
+	}}, nil
+}

thxCode/acs-terway

cmd/terway-controlplane/terway-controlplane.go
@@ -1,0 +1,184 @@
+/*
+Copyright 2021 Terway Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	goflag "flag"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/AliyunContainerService/terway/pkg/aliyun"
+	"github.com/AliyunContainerService/terway/pkg/apis/crds"
+	networkv1beta1 "github.com/AliyunContainerService/terway/pkg/apis/network.alibabacloud.com/v1beta1"
+	"github.com/AliyunContainerService/terway/pkg/cert"
+	register "github.com/AliyunContainerService/terway/pkg/controller"
+	_ "github.com/AliyunContainerService/terway/pkg/controller/all"
+	"github.com/AliyunContainerService/terway/pkg/controller/vswitch"
+	"github.com/AliyunContainerService/terway/pkg/controller/webhook"
+	"github.com/AliyunContainerService/terway/pkg/logger"
+	"github.com/AliyunContainerService/terway/pkg/utils"
+	"github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/pkg/version"
+	"k8s.io/klog/v2/klogr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+)
+
+var (
+	leaseLockName      string
+	leaseLockNamespace string
+	healthzBindAddress string
+
+	scheme = runtime.NewScheme()
+	log    = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	flag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+
+	flag.StringVar(&leaseLockName, "lease-lock-name", "terway-controller-lock", "the lease lock resource name")
+	flag.StringVar(&leaseLockNamespace, "lease-lock-namespace", "kube-system", "the lease lock resource namespace")
+	flag.StringVar(&healthzBindAddress, "healthzBindAddress", "0.0.0.0:80", "for health check")
+
+	flag.String("cluster-id", "", "cluster id for the cluster")
+	flag.String("vpc-id", "", "vpc id for the cluster")
+
+	flag.String("cluster-domain", "cluster.local", "domain for the cluster")
+	flag.String("controller-namespace", "kube-system", "specific controller run namespace")
+	flag.String("controller-name", "terway-controlplane", "specific controller name")
+	flag.String("cert-dir", "/var/run/webhook-cert", "webhook cert dir")
+	flag.Int("webhook-port", 443, "port for webhook")
+
+	_ = viper.BindPFlag("cluster-id", flag.CommandLine.Lookup("cluster-id"))
+	_ = viper.BindPFlag("vpc-id", flag.CommandLine.Lookup("vpc-id"))
+
+	_ = viper.BindPFlag("cluster-domain", flag.CommandLine.Lookup("cluster-domain"))
+	_ = viper.BindPFlag("controller-namespace", flag.CommandLine.Lookup("controller-namespace"))
+	_ = viper.BindPFlag("controller-name", flag.CommandLine.Lookup("controller-name"))
+	_ = viper.BindPFlag("cert-dir", flag.CommandLine.Lookup("cert-dir"))
+	_ = viper.BindPFlag("webhook-port", flag.CommandLine.Lookup("webhook-port"))
+
+	_ = viper.BindPFlag("v", flag.CommandLine.Lookup("v"))
+
+	_ = viper.BindEnv("ALIBABA_CLOUD_REGION_ID")
+	_ = viper.BindEnv("ALIBABA_CLOUD_ACCESS_KEY_ID")
+	_ = viper.BindEnv("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
+
+	viper.SetDefault("v", 5)
+
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(networkv1beta1.AddToScheme(scheme))
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	if viper.GetInt("v") > len(logrus.AllLevels) {
+		_ = logger.SetLevel(logrus.AllLevels[len(logrus.AllLevels)-1].String())
+	} else {
+		_ = logger.SetLevel(logrus.AllLevels[viper.GetInt("v")].String())
+	}
+
+	ctrl.SetLogger(klogr.New())
+	log.Info(fmt.Sprintf("GitCommit %s BuildDate %s Platform %s",
+		version.Get().GitCommit, version.Get().BuildDate, version.Get().Platform))
+
+	flag.Parse()
+
+	utils.RegisterClients()
+
+	err := crds.RegisterCRDs()
+	if err != nil {
+		panic(err)
+	}
+
+	err = cert.SyncCert()
+	if err != nil {
+		panic(err)
+	}
+
+	err = utils.ParseClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Info("init cluster config", "clusterID", viper.GetString("cluster-id"), "vpcID", viper.GetString("vpc-id"))
+
+	aliyunClient, err := aliyun.NewAliyun(viper.GetString("ALIBABA_CLOUD_ACCESS_KEY_ID"), viper.GetString("ALIBABA_CLOUD_ACCESS_KEY_SECRET"), viper.GetString("ALIBABA_CLOUD_REGION_ID"), "")
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := ctrl.SetupSignalHandler()
+	restConfig := ctrl.GetConfigOrDie()
+
+	mgr, err := ctrl.NewManager(restConfig, ctrl.Options{
+		Scheme:                     scheme,
+		HealthProbeBindAddress:     healthzBindAddress,
+		Host:                       "0.0.0.0",
+		Port:                       viper.GetInt("webhook-port"),
+		CertDir:                    viper.GetString("cert-dir"),
+		LeaderElection:             true,
+		LeaderElectionID:           viper.GetString("controller-name"),
+		LeaderElectionNamespace:    viper.GetString("controller-namespace"),
+		LeaderElectionResourceLock: "leases",
+		MetricsBindAddress:         "0",
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = mgr.AddHealthzCheck("healthz", healthz.Ping)
+	if err != nil {
+		panic(err)
+	}
+	err = mgr.AddReadyzCheck("readyz", healthz.Ping)
+	if err != nil {
+		panic(err)
+	}
+
+	mgr.GetWebhookServer().Register("/mutating", webhook.MutatingHook(mgr.GetClient()))
+	mgr.GetWebhookServer().Register("/validate", webhook.ValidateHook())
+
+	vSwitchCtrl, err := vswitch.NewSwitchPool(aliyunClient)
+	if err != nil {
+		panic(err)
+	}
+	err = mgr.Add(vSwitchCtrl)
+	if err != nil {
+		panic(err)
+	}
+
+	for name := range register.Controllers {
+		err = register.Controllers[name](mgr, aliyunClient, vSwitchCtrl)
+		if err != nil {
+			panic(err)
+		}
+		log.Info("register controller", "controller", name)
+	}
+
+	log.Info("controller started")
+	err = mgr.Start(ctx)
+	if err != nil {
+		panic(err)
+	}
+}

pkg/cert/webhook.go
@@ -1,0 +1,248 @@
+package cert
+
+import (
+	"bytes"
+	"context"
+	cryptorand "crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/AliyunContainerService/terway/pkg/utils"
+
+	"github.com/spf13/viper"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+const (
+	serverCertKey = "tls.crt"
+	serverKeyKey  = "tls.key"
+
+	caCertKey = "ca.crt"
+)
+
+// SyncCert sync cert for webhook
+func SyncCert() error {
+
+	cs := utils.K8sClient
+	// check secret
+	var serverCertBytes, serverKeyBytes, caCertBytes []byte
+
+	ns := viper.GetString("controller-namespace")
+	name := viper.GetString("controller-name")
+
+	// get cert from secret or generate it
+	existSecret, err := cs.CoreV1().Secrets(ns).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		if !errors.IsNotFound(err) {
+			return fmt.Errorf("error get cert from secret, %w", err)
+		}
+		// create certs
+		s, err := GenerateCerts(ns, name, viper.GetString("cluster-domain"))
+		if err != nil {
+			return fmt.Errorf("error generate cert, %w", err)
+		}
+
+		serverCertBytes = s.Data[serverCertKey]
+		serverKeyBytes = s.Data[serverKeyKey]
+		caCertBytes = s.Data[caCertKey]
+		s.Name = name
+		s.Namespace = ns
+		// create secret this make sure one is the leader
+		_, err = cs.CoreV1().Secrets(ns).Create(context.Background(), s, metav1.CreateOptions{})
+		if err != nil {
+			if !errors.IsAlreadyExists(err) {
+				return fmt.Errorf("error create cert to secret, %w", err)
+			}
+			secret, err := cs.CoreV1().Secrets(ns).Get(context.Background(), name, metav1.GetOptions{})
+			if err != nil {
+				return fmt.Errorf("error get cert from secret, %w", err)
+			}
+			serverCertBytes = secret.Data[serverCertKey]
+			serverKeyBytes = secret.Data[serverKeyKey]
+			caCertBytes = secret.Data[caCertKey]
+		}
+	} else {
+		serverCertBytes = existSecret.Data[serverCertKey]
+		serverKeyBytes = existSecret.Data[serverKeyKey]
+		caCertBytes = existSecret.Data[caCertKey]
+	}
+
+	certDir := viper.GetString("cert-dir")
+	err = os.MkdirAll(certDir, os.ModeDir)
+	if err != nil {
+		return err
+	}
+
+	// write cert to file
+	err = os.WriteFile(filepath.Join(certDir, serverCertKey), serverCertBytes, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error create secret file, %w", err)
+	}
+	err = os.WriteFile(filepath.Join(certDir, serverKeyKey), serverKeyBytes, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error create secret file, %w", err)
+	}
+	err = os.WriteFile(filepath.Join(certDir, caCertKey), caCertBytes, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error create secret file, %w", err)
+	}
+
+	// update webhook
+	mutatingWebhook, err := cs.AdmissionregistrationV1().MutatingWebhookConfigurations().Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	if len(mutatingWebhook.Webhooks) == 0 {
+		return fmt.Errorf("no webhook config found")
+	}
+	mutatingCfg := mutatingWebhook.DeepCopy()
+	for i, hook := range mutatingWebhook.Webhooks {
+		if len(hook.ClientConfig.CABundle) != 0 {
+			continue
+		}
+		// patch ca
+		mutatingCfg.Webhooks[i].ClientConfig.CABundle = caCertBytes
+	}
+	mutatPatchBytes, err := json.Marshal(mutatingCfg)
+	if err != nil {
+		return err
+	}
+	err = wait.ExponentialBackoff(utils.DefaultPatchBackoff, func() (done bool, err error) {
+		_, innerErr := cs.AdmissionregistrationV1().MutatingWebhookConfigurations().Patch(context.Background(), mutatingWebhook.Name, types.StrategicMergePatchType, mutatPatchBytes, metav1.PatchOptions{})
+		if innerErr != nil {
+			return false, err
+		}
+		return true, nil
+	})
+	if err != nil {
+		return err
+	}
+
+	validateWebhook, err := cs.AdmissionregistrationV1().ValidatingWebhookConfigurations().Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	if len(validateWebhook.Webhooks) == 0 {
+		return fmt.Errorf("no webhook config found")
+	}
+	validateCfg := validateWebhook.DeepCopy()
+	for i, hook := range validateWebhook.Webhooks {
+		if len(hook.ClientConfig.CABundle) != 0 {
+			continue
+		}
+		// patch ca
+		validateCfg.Webhooks[i].ClientConfig.CABundle = caCertBytes
+	}
+	validatePatchBytes, err := json.Marshal(validateCfg)
+	if err != nil {
+		return err
+	}
+	err = wait.ExponentialBackoff(utils.DefaultPatchBackoff, func() (done bool, err error) {
+		_, innerErr := cs.AdmissionregistrationV1().ValidatingWebhookConfigurations().Patch(context.Background(), validateWebhook.Name, types.StrategicMergePatchType, validatePatchBytes, metav1.PatchOptions{})
+		if innerErr != nil {
+			return false, err
+		}
+		return true, nil
+	})
+
+	return err
+}
+
+func GenerateCerts(serviceNamespace, serviceName, clusterDomain string) (*corev1.Secret, error) {
+	var caPEM, serverCertPEM, serverPrivateKeyPEM *bytes.Buffer
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(2021),
+		Subject: pkix.Name{
+			Organization: []string{clusterDomain},
+		},
+		NotBefore:             time.Now().Add(-time.Hour),
+		NotAfter:              time.Now().AddDate(100, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	caPK, err := rsa.GenerateKey(cryptorand.Reader, 4096)
+	if err != nil {
+		return nil, err
+	}
+
+	caBytes, err := x509.CreateCertificate(cryptorand.Reader, ca, ca, &caPK.PublicKey, caPK)
+	if err != nil {
+		return nil, err
+	}
+
+	caPEM = new(bytes.Buffer)
+	err = pem.Encode(caPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: caBytes,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	commonName := fmt.Sprintf("%s.%s.svc", serviceName, serviceNamespace)
+	dnsNames := []string{serviceName,
+		fmt.Sprintf("%s.%s", serviceName, serviceNamespace),
+		commonName}
+
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(2021),
+		DNSNames:     dnsNames,
+		Subject: pkix.Name{
+			CommonName:   commonName,
+			Organization: []string{clusterDomain},
+		},
+		NotBefore:   time.Now().Add(-time.Hour),
+		NotAfter:    time.Now().AddDate(100, 0, 0),
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:    x509.KeyUsageDigitalSignature,
+	}
+
+	serverPrivateKey, err := rsa.GenerateKey(cryptorand.Reader, 4096)
+	if err != nil {
+		return nil, err
+	}
+
+	serverCertBytes, err := x509.CreateCertificate(cryptorand.Reader, cert, ca, &serverPrivateKey.PublicKey, caPK)
+	if err != nil {
+		return nil, err
+	}
+
+	serverCertPEM = new(bytes.Buffer)
+	err = pem.Encode(serverCertPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: serverCertBytes,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	serverPrivateKeyPEM = new(bytes.Buffer)
+	err = pem.Encode(serverPrivateKeyPEM, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(serverPrivateKey),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &corev1.Secret{Data: map[string][]byte{
+		caCertKey:     caPEM.Bytes(),
+		serverCertKey: serverCertPEM.Bytes(),
+		serverKeyKey:  serverPrivateKeyPEM.Bytes(),
+	}}, nil
+}